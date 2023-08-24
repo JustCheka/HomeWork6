2f6357f4642509a8a0ded6edcbe998118dab9e84
JustCheka <cheka201@yandxe.ru>
2023-08-24 23:47:47 +0300

    Ex 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintArr(int[] arr){
+    foreach(var item in arr){
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Сколько будет чисел");
+int m = int.Parse(Console.ReadLine());
+
+int[] arr = new int[m];
+
+int sum = 0;
+for (int i = 0; i < arr.Length; i++){
+    Console.Write($"{i + 1} число = ");
+    arr[i] = int.Parse(Console.ReadLine());
+    if (arr[i] > 0) sum++;
+}
+Console.WriteLine();
+PrintArr(arr);
+Console.WriteLine($"количество чисел больше 0 = {sum}");
