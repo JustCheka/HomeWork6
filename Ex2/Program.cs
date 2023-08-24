edc98eee72f4f810be016dfe9d6d6d170f3605dc
JustCheka <cheka201@yandxe.ru>
2023-08-24 23:58:33 +0300

    Ex 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите b1 ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите k1 ");
+int k1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите b2 ");
+int b2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите k2 ");
+int k2 = int.Parse(Console.ReadLine());
+
+double result = ((double)b2 - b1) / (k1 - k2);
+Console.WriteLine(result);
